463415000f91a4a71765e67843d8642832d8795b
Anthony <anthony.onyango@student.moringaschool.com>
2024-07-03 19:11:44 +0300

    update index.js

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // Code your solution here
+
+
+// findMatching
+function findMatching(source, sought) {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase() ===sought.toLowerCase()
+    );
+
+}
+
+//fuzzyMatch
+function fuzzyMatch(source, sought) {
+    return source.filter(
+        (possibleMatch) => possibleMatch.toLowerCase().indexOf(sought.toLowerCase()) ===0
+    );
+}
+
+//matchName
+function matchName(source, sought) {
+    return source.filter((record) => record.name === sought)
+}
